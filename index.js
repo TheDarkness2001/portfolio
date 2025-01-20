088a6575935dae79a512b9bd8c2954db05523dc3
Malik <124065927+TheDarkness2001@users.noreply.github.com>
2025-01-20 10:37:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+        var typed = new Typed(".auto-type", {
+            strings: ["TheDarkness' website", "Khusanboy's website", "Malik's website"],
+            typeSpeed: 170,
+            backSpeed: 170,
+            loop: true,
+        });
+
+      
